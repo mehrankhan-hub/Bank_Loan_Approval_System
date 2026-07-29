7d88c8450e88cfcf58b51dd49d221412de7ede5b
mehrankhan-hub <imehran.pk@gmail.com>
2026-07-29 00:28:57 -0700

    Add files via upload

---
+++ DecisionTree.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import pickle as pkl
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv('bankloan.csv')
+pd.set_option('display.max_columns', None)
+#print(df)
+df['Experience'] = df['Experience'].abs()
+X = df[["Age","Experience","Income","Family","CCAvg","Education","Mortgage",
+        "Securities_Account","CD_Account","Online","CreditCard"]]
+y = df["Personal_Loan"]
+
+#data splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+#Using Decision Tree
+model = DecisionTreeClassifier(max_depth=4,random_state=42)
+model.fit(X_train,y_train)
+score = model.score(X_test,y_test)
+print(score)
+
+with open('bankloan.pkl', 'wb') as f:
+    pkl.dump(model, f)
+
+with open('Accuracy.pkl', 'wb') as f:
+    pkl.dump(score,f)
